89641b21995aaa6ee5df555139813d0b9594f6f1
Abhi <abhinuspotify@gmail.com>
2024-12-16 20:40:30 -0500

    build testing

--- test/news_test.py
+++ test/news_test.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def test_news_data_fetching():
-    assert len(API_KEY)>5
+    assert len(os.environ.get("NEWSAPI_KEY"))>5
     response = fetch_news_csv("sports", "DataFrame")
     assert isinstance(response, DataFrame)
     assert set(response.columns) == {"author", "title", "description", "url", "publishedAt"}
