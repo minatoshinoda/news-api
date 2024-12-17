ad561d162ceda8fdd05dd82a0803145f66ea3882
Abhi <abhinuspotify@gmail.com>
2024-12-16 20:18:16 -0500

    build failure debugging continued

--- test/news_test.py
+++ test/news_test.py
@@ -1,9 +1,13 @@
 from pandas import DataFrame
 from app.news_service import API_KEY
 from app.news import fetch_news_csv
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
 
 def test_news_data_fetching():
-    assert len(API_KEY)>5
+    assert len(os.getenv("NEWSAPI_KEY"))>5
     response = fetch_news_csv("sports", "DataFrame")
     assert isinstance(response, DataFrame)
     #assert set(response.columns) == {"author", "title", "description", "url", "publishedAt"}
